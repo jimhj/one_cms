fa9060f41c9317912ae970c359bc75cb89e2550e
jimmy <jimmy.huangjin@gmail.com>
2015-11-06 22:37:04 +0800

    suppirt infinite levels of node

--- app/helpers/admin/nodes_helper.rb
+++ app/helpers/admin/nodes_helper.rb
@@ -1,26 +1,25 @@
 module Admin
   module NodesHelper
-    def node_options
-      result = []
-      items = Node.roots || []
-      items.each do |root|
-        result += root.class.associate_parents(root.self_and_descendants).map do |i|
-          option = "<option value='#{i.id}'"
-          if @node.parent_id == i.id
-            option += " selected=selected"
-          end
-
-          if i.level.zero?
-            option += ">#{i.name}"
-          else
-            option += " style='text-indent: #{i.level*10}px'>"
-            option += "﹂#{i.name}"
-          end
-
-          option += "</option>"
+    def node_list(children, reserve = '')
+      children.each do |child|
+        reserve += %Q(
+          <div class="node-item"><div class="inner clearfix">
+            <div class="pull-left expander"><a href="#"><img src="#{image_path('admin/contract.gif')}" /></a></div>
+            <div class="pull-left node-name">#{child.name} [id: #{child.id}]</div>
+            <div class="pull-right ops">
+              <a href="#">编辑</a><span>|</span>
+              <a href="#">添加子栏目</a><span>|</span>
+              <a href="#">删除</a>
+            </div>
+          </div>
+        )
+        if child.children.count > 0
+          node_list(child.children, reserve)
+        else
+          reserve+="</div>" * child.level
         end
       end
-      result
+      reserve
     end
   end
 end
