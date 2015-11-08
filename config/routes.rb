922e02b5fb2d29ce4e708fa2aa85b7606cd3f18f
jimmy <jimmy.huangjin@gmail.com>
2015-11-08 22:10:39 +0800

    secure admin panel route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RedactorRails::Engine => '/redactor_rails'
   mount RuCaptcha::Engine => '/rucaptcha'
 
-  namespace :admin do
+  namespace :onecmsmanage, module: :admin, as: :admin do
     get :login, to: 'sessions#new'
     post :login, to: 'sessions#create'
     resources :articles
