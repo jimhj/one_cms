40da8ca47715aa98bcc3fdb373dadd8039dbb1e9
jimmy <jimmy.huangjin@gmail.com>
2015-11-08 21:24:25 +0800

    check if article contains keywords

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -17,6 +17,17 @@ class Admin::ArticlesController < Admin::ApplicationController
     end
   end
 
+  def show
+    @article = Article.find params[:id]
+  end
+
+  def destroy
+    @article = Article.find params[:id]
+    @article.destroy
+
+    redirect_to admin_articles_path
+  end
+
   private
 
   def article_params
--- app/models/article_body.rb
+++ app/models/article_body.rb
@@ -5,5 +5,12 @@ class ArticleBody < ActiveRecord::Base
   before_save do
     # 1.提取关键词，加上内链
     # 2.把远程图片下载到本地
+    self.body_html ||= self.body
+    keywords = Keyword.select(:name, :url).each do |keyword|
+      self.body_html = self.body_html.sub(
+        /#{keyword.name}/,
+        %Q(<a href="#{keyword.url}" target="_blank" title="#{keyword.name}">#{keyword.name}</a>)
+      )
+    end
   end
 end
