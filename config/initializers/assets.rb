e577af805b8cfa63fe80990dd88c953786605462
jimmy <jimmy.huangjin@gmail.com>
2015-12-22 22:56:29 +0800

    add right box

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,8 @@ Rails.application.config.assets.precompile += %w(
   admin/app.js
   admin/app.css
   h4ui/site.js
+  h4ui/owl/owl.carousel.css
+  h4ui/owl/owl.carousel.js
+  index.scss
   site.css
 )
