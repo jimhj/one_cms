f23f41792f71df2b1b4d7668d74ecdfa5ca081ff
jimmy <jimmy.huangjin@gmail.com>
2016-01-10 20:32:35 +0800

    refine article index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,9 @@ module ApplicationHelper
     "/#{article.node.slug}/#{article.id}"
   end
 
-  def main_nav_class
-    
+  def render_right_nav(node)
+     node.children.first(8).collect do |child|
+      "<a href='#{articles_path(child.slug)}' title='#{child.name}'>#{child.name}</a>"
+    end.join('<span>-</span>').html_safe
   end
 end
