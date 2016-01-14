50b13556e4c3ce59c8653d6bf9b1913f50770bd4
jimmy <jimmy.huangjin@gmail.com>
2016-01-14 10:33:35 +0800

    fixbug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-
-CarrierWave.configure do |config|
-  config.asset_host = Setting.carrierwave.asset_host
-end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,3 @@
-carrierwave_config = CONFIG['carrierwave']
-
 CarrierWave.configure do |config|
-  config.asset_host = carrierwave_config['asset_host']
+  config.asset_host = Setting.carrierwave.asset_host
 end
