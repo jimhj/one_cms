70d15f4a87ac8e9463f7024b742f566f3f4f40b3
jimmy <jimmy.huangjin@gmail.com>
2016-01-20 16:59:53 +0800

    mobile tags pages render data

--- app/controllers/mobile/tags_controller.rb
+++ app/controllers/mobile/tags_controller.rb
@@ -1,8 +1,10 @@
 class Mobile::TagsController < Mobile::ApplicationController
   def index
+    @tags = Tag.order('id DESC').paginate(page: params[:page], per_page: 60)
   end
 
   def show
-    @articles = Article.order('id DESC').paginate(page: params[:page], per_page: 20)
+    @tag = Tag.find_by slug: params[:id]
+    @articles = @tag.articles.order('id DESC').paginate(page: params[:page], per_page: 20)
   end
 end
