9de0e7ae9b12d079f5415cf396f0897379942fb4
jimmy <jimmy.huangjin@gmail.com>
2016-01-20 17:41:32 +0800

    render mobile pages data

--- app/controllers/mobile/articles_controller.rb
+++ app/controllers/mobile/articles_controller.rb
@@ -1,5 +1,8 @@
 class Mobile::ArticlesController < Mobile::ApplicationController
   def index
+    @node = Node.find_by(slug: params[:slug])
+    node_ids = @node.self_and_descendants.pluck(:id)
+    @articles = Article.where(node_id: node_ids).order('id DESC').paginate(page: params[:page], per_page: 20)
   end
 
   def show
--- app/controllers/mobile/channels_controller.rb
+++ app/controllers/mobile/channels_controller.rb
@@ -1,9 +1,10 @@
 class Mobile::ChannelsController < Mobile::ApplicationController
   def index
-    
+   @channels = Channel.order('sortrank DESC, id DESC').paginate(page: params[:page], per_page: 20)    
   end
 
   def show
-    
+    @channel = Channel.find_by slug: params[:slug]
+    @articles = @channel.articles.order('id DESC').paginate(page: params[:page], per_page: 20)
   end
 end
