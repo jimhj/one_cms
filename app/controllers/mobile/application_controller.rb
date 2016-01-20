b2db244fbddc2435d372133e0d5cc9031715bb6d
jimmy <jimmy.huangjin@gmail.com>
2016-01-20 22:29:02 +0800

    adjust page style

--- app/controllers/mobile/application_controller.rb
+++ app/controllers/mobile/application_controller.rb
@@ -1,7 +1,8 @@
 class Mobile::ApplicationController < ApplicationController
   layout 'mobile'
   
+  caches_action :index, expires_in: 1.hour, cache_path: 'mobile/index'
+
   def index
-    
   end
 end
