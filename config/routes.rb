5ccb32ac293acb48f2bde551493a3a31e783bae2
jimmy <jimmy.huangjin@gmail.com>
2016-01-24 21:42:13 +0800

    fixbug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 class MobileConstraint
   def self.matches?(request)
     # true
-    (request.user_agent.to_s =~ /Mobile|webOS/) or request.subdomain == 'm'
+    (request.user_agent.to_s =~ /Mobile|webOS/) or request.subdomain == 'm' or request.domain == 'm.h4.com.cn'
   end
 end
 
