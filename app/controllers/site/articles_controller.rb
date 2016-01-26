c2c15528a04a2ec19962f1770e9693e71a2073a2
jimmy <jimmy.huangjin@gmail.com>
2016-01-26 22:35:53 +0800

    add search

--- app/controllers/site/articles_controller.rb
+++ app/controllers/site/articles_controller.rb
@@ -31,6 +31,11 @@ class Site::ArticlesController < Site::ApplicationController
     fresh_when(etag: [@article, Keyword.recent], template: false) 
   end
 
+  def search
+    url = "http://s.h4.com.cn/cse/search?q=#{CGI::escape(params[:q])}&click=1&s=14346881451190138661&nsid=1"
+    redirect_to url
+  end
+
   def feed
     @articles = Article.includes(:article_body, :node).order('id DESC').limit(20)
     render layout: false
