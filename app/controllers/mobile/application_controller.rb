ff8b2f6b2690e3898fd7be2764ae52c3c61d1a4c
jimmy <jimmy.huangjin@gmail.com>
2016-02-01 22:49:28 +0800

    refine

--- app/controllers/mobile/application_controller.rb
+++ app/controllers/mobile/application_controller.rb
@@ -1,9 +1,9 @@
 class Mobile::ApplicationController < ApplicationController
   layout 'mobile'
   
-  caches_action :index, cache_path: 'mobile/index', expires_in: 2.hours, if: Proc.new {
+  caches_action :index, cache_path: 'mobile/index' if: Proc.new {
     controller_name == 'application'
-  }
+  }, expires_in: 2.hours
 
   def index
     @links = Link.where(linkable_id: 0).mobile
--- app/controllers/site/application_controller.rb
+++ app/controllers/site/application_controller.rb
@@ -1,9 +1,9 @@
 class Site::ApplicationController < ApplicationController
   layout 'site'
 
-  caches_action :index, cache_path: 'desktop/index', expires_in: 2.hours, if: Proc.new {
+  caches_action :index, cache_path: 'desktop/index', if: Proc.new {
     controller_name == 'application'
-  }
+  }, :expires_in => 2.hours
 
   def index
     @links = Link.where(linkable_id: 0).pc
