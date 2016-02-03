29a028fcf82f9cbc4d754341407f386267bb0eaf
jimmy <jimmy.huangjin@gmail.com>
2016-02-03 21:27:33 +0800

    refine keyword

--- app/models/article_body.rb
+++ app/models/article_body.rb
@@ -19,32 +19,54 @@ class ArticleBody < ActiveRecord::Base
     end
   end
 
+  # def replace_keywords
+  #   keywords = Keyword.order('id DESC, sortrank DESC').select(:id, :name, :url)
+  #   # if not cached_keyword_id.zero?
+  #   #   keywords = keywords.where('id > ?', cached_keyword_id)
+  #   # end
+  #   # keywords = keywords.limit(5000)
+
+  #   # return self.body_html if keywords.blank?
+    
+  #   keywords.each do |keyword|
+  #     doc = Nokogiri::HTML(self.body_html.presence || self.body)
+  #     ele = doc.xpath("//*[contains(text(), '#{keyword.name}')]").first
+  #     next if ele.nil?
+  #     next if ele.name == 'a'
+  #     link = Nokogiri::XML::Node.new "a", doc
+  #     link.set_attribute(:href, keyword.url)
+  #     link.set_attribute(:css, 'hot-link')
+  #     link.set_attribute(:target, '_blank')
+  #     link.set_attribute(:title, keyword.name)
+  #     link.content = keyword.name
+  #     ele.content = ele.content.sub(/#{keyword.name}/, link.to_html)
+  #     ele.inner_html = ele.content.to_s
+  #     self.body_html = doc.to_html
+  #   end
+
+  #   # update_columns(cached_keyword_id: keywords.first.try(:id) || 0, body_html: doc.to_html)
+
+  #   self.body_html
+  # end
+
   def replace_keywords
-    keywords = Keyword.order('id DESC').select(:id, :name, :url)
+    keywords = Keyword.select(:id, :name, :url)
     if not cached_keyword_id.zero?
       keywords = keywords.where('id > ?', cached_keyword_id)
     end
     keywords = keywords.limit(5000)
-
-    return self.body_html if keywords.blank?
     
-    doc = Nokogiri::HTML(self.body_html.presence || self.body)
-    keywords.each do |keyword|
-      ele = doc.xpath("//p[contains(text(), '#{keyword.name}')]").first
-      next if ele.nil?
-      link = Nokogiri::XML::Node.new "a", doc
-      link.set_attribute(:href, keyword.url)
-      link.set_attribute(:css, 'hot-link')
-      link.set_attribute(:target, '_blank')
-      link.set_attribute(:title, keyword.name)
-      link.content = keyword.name
-      ele.inner_html = ele.content.gsub(/#{keyword.name}/, link.to_html)
+    return self.body_html if keywords.blank?
+
+    keywords.each do |kw|
+      link = "<a href='#{kw.url}' class='hot-link' target='_blank'>#{kw.name}</a>"
+      self.body_html = (self.body_html.presence || self.body).sub(/#{kw.name}/, link)
     end
 
-    update_columns(cached_keyword_id: keywords.first.try(:id) || 0, body_html: doc.to_html)
-
-    doc.to_html
+    update_columns(cached_keyword_id: keywords.first.try(:id) || 0, body_html: self.body_html)
+    self.body_html
   end
+
 
   def restore_remote_images
     doc = Nokogiri::HTML(self.body)
--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,6 +2,10 @@ class Keyword < ActiveRecord::Base
   validates_presence_of :name, :url, :sortrank
   validates_uniqueness_of :name
 
+  default_scope {
+    order('sortrank DESC, LENGTH(name) DESC, id DESC')
+  }
+
   def self.recent
     self.order('created_at DESC, updated_at DESC').limit(1).first
   end
