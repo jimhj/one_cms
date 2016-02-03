aa2cc1fce83c85c2efc1d30bd2d92b3beaa9646e
jimmy <jimmy.huangjin@gmail.com>
2016-02-03 21:59:39 +0800

    bugfix

--- app/models/article_body.rb
+++ app/models/article_body.rb
@@ -50,7 +50,7 @@ class ArticleBody < ActiveRecord::Base
   # end
 
   def replace_keywords
-    keywords = Keyword.select(:id, :name, :url)
+    keywords = Keyword.order('id DESC').select(:id, :name, :url)
     if not cached_keyword_id.zero?
       keywords = keywords.where('id > ?', cached_keyword_id)
     end
--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,9 +2,9 @@ class Keyword < ActiveRecord::Base
   validates_presence_of :name, :url, :sortrank
   validates_uniqueness_of :name
 
-  default_scope {
-    order('sortrank DESC, LENGTH(name) DESC, id DESC')
-  }
+  # default_scope {
+  #   order('sortrank DESC, LENGTH(name) DESC, id DESC')
+  # }
 
   def self.recent
     self.order('created_at DESC, updated_at DESC').limit(1).first
