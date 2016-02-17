64c6c3d78fa429bb0d122dc303a5bad0d905c664
jimmy <jimmy.huangjin@gmail.com>
2016-02-17 17:29:39 +0800

    reduce inner link check

--- app/models/article_body.rb
+++ app/models/article_body.rb
@@ -24,7 +24,7 @@ class ArticleBody < ActiveRecord::Base
     if not cached_keyword_id.zero?
       keywords = keywords.where('id > ?', cached_keyword_id)
     end
-    keywords = keywords.limit(10000)
+    keywords = keywords.limit(5000)
 
     if keywords.blank?
       return self.body_html || self.body
