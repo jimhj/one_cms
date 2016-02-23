39e9bedef840205fe28f250a83e287d27ef21d24
jimmy <jimmy.huangjin@gmail.com>
2016-02-23 16:37:44 +0800

    update delayed_job

--- config/initializers/delayed_job_web.rb
+++ config/initializers/delayed_job_web.rb
@@ -1,4 +1,5 @@
 Delayed::Worker.max_attempts = 1
+Delayed::Worker.destroy_failed_jobs = true
 
 DelayedJobWeb.use Rack::Auth::Basic do |username, password|
   username == Setting.delayed_job_web.user && password == Setting.delayed_job_web.password
