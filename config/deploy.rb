d30582f0b387db411ed402dd2818b1ab69584fb9
jimmy <jimmy.huangjin@gmail.com>
2016-04-09 18:13:08 +0800

    refine search

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'msj'
+set :application, 'h4'
 set :deploy_to, "~/www/#{fetch(:application)}/"
 set :repo_url, 'git@github.com:jimhj/one_cms.git'
 
