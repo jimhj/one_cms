cd1739340c3c997027ce182ea4bcad1cc5784d86
jimmy <jimmy.huangjin@gmail.com>
2016-04-15 11:13:30 +0800

    update asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,9 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  if Setting.qiniu.active && Setting.qiniu.mirror_on
+    config.action_controller.asset_host = Setting.qiniu.mirror_host
+  end
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
