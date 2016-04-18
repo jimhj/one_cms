b3804c295763fd336081f8d932fefc5657968abe
jimmy <jimmy.huangjin@gmail.com>
2016-04-18 21:06:14 +0800

    refine

--- app/controllers/mobile/application_controller.rb
+++ app/controllers/mobile/application_controller.rb
@@ -3,11 +3,12 @@ class Mobile::ApplicationController < ApplicationController
 
   # self.page_cache_directory = Rails.root.join('public/page_cache/mobile').to_s
 
-  caches_action :index, :cache_path => Proc.new { |c| c.request.url + '-mobile-index' }, :expires_in => 2.hours
+  # caches_action :index, :cache_path => Proc.new { |c| c.request.url + '-mobile-index' }, :expires_in => 2.hours
 
-  # caches_page :index, if: Proc.new {
-  #   controller_name == 'application'
-  # }
+  caches_action :index, cache_path: => Proc.new { |c| c.request.url + '-mobile-index' }, if: Proc.new {
+    controller_name == 'application'
+  }, :expires_in => 2.hours
+
 
   def index
     @articles = Article.recommend(page: params[:page])
