2f92d645d377b06e3913c20cc726f2ee4a6e606b
jimmy <jimmy.huangjin@gmail.com>
2017-05-25 21:16:46 +0800

    bugfix

--- app/controllers/site/articles_controller.rb
+++ app/controllers/site/articles_controller.rb
@@ -29,10 +29,10 @@ class Site::ArticlesController < Site::ApplicationController
     tag_ids = @article.taggings.pluck(:tag_id)
     if tag_ids.any?
       article_ids = Tagging.where(tag_id: tag_ids).order('id DESC').limit(8).pluck(:article_id)
-      @more_articles = Article.where(id: article_ids).order('id DESC')
+      @more_articles = Article.where(id: article_ids).order('id DESC').limit(8).to_a
 
       if (ct = @more_articles.count) < 8
-        more = Article.where(node_id: @nodes.pluck(:id)).limit(8 - ct)
+        more = Article.where(node_id: @nodes.pluck(:id)).limit(8 - ct).to_a
         @more_articles = @more_articles + more
       end
     else
